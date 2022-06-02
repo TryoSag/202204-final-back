85314900eed14748c40db0a55065a21799af96f7
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-02 17:31:16 +0200

    added test on getPets function

---
+++ src/server/controllers/petsControllers.test.js
@@ -0,0 +1,21 @@
+const Pet = require("../../database/models/Pet");
+const getPets = require("./petsControllers");
+
+describe("Given the getPets function", () => {
+  describe("When it's called and there are pets in database", () => {
+    test("Then it should call response with status 200 and json group of pets", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const groupOfPets = ["pet1", "pet2", "pet3"];
+      Pet.find = jest.fn().mockResolvedValue(groupOfPets);
+      const expectedStatus = 200;
+
+      await getPets(null, res);
+
+      expect(res.status).toBeCalledWith(expectedStatus);
+      expect(res.json).toBeCalledWith(groupOfPets);
+    });
+  });
+});
